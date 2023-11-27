5b98ce2c8d1b5f2c51e1d2c99fc3985be708c03d
figlesias221 <figlesias221@gmail.com>
2023-11-27 11:36:42 -0300

    Fix

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if "messages" not in st.session_state.keys():
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading data..."):
-        pinecone_index = pinecone.Index("tailwind-ada")
+        pinecone_index = pinecone.Index("tailwind-hugging")
         vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on TailwindCSS and your job is to answer technical questions. Assume that all questions are related to TailwindCSS. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_vector_store(vector_store=vector_store, service_context=service_context)
--- streamlit_app.py
+++ streamlit_app.py
@@ -14,21 +14,11 @@ if "messages" not in st.session_state.keys():
         {"role": "assistant", "content": "Hello, I'm Tailwind GPT. How can I help you?"}
     ]
 
-@st.cache_resource(show_spinner=False)
-def load_data():
-    with st.spinner(text="Loading data..."):
-        pinecone_index = pinecone.Index("tailwind-ada")
-        vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on TailwindCSS and your job is to answer technical questions. Assume that all questions are related to TailwindCSS. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_vector_store(vector_store=vector_store, service_context=service_context)
-        return index
-   
-
-index = load_data()
-
-# index.as_query_engine()
+pinecone_index = pinecone.Index("tailwind-hugging")
+vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on TailwindCSS and your job is to answer technical questions. Assume that all questions are related to TailwindCSS. Keep your answers technical and based on facts – do not hallucinate features."))
+index = VectorStoreIndex.from_vector_store(vector_store=vector_store, service_context=service_context)
 chat_engine = index.as_chat_engine(chat_mode="react", verbose=True)
-
 
 if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
